396a911a962c00f0966e00d532decb2c0f482bff
Vibhav <vibhavsachan743@gmail.com>
2023-02-19 18:36:13 +0530

    SignUp

---
+++ app/src/main/java/com/example/foodshala/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.foodshala
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/com/example/foodshala/SplashScreen.kt
@@ -0,0 +1,27 @@
+package com.example.foodshala
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
+
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1000L)
+            startActivity(Intent(this@SplashScreen,SignUp::class.java))
+            finish()
+        }
+    }
+}
