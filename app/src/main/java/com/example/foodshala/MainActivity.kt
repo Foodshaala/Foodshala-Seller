c36a5f992db7a55a0f6fd80e8ecdd1503b191b95
Vibhav <vibhavsachan743@gmail.com>
2023-02-25 18:25:59 +0530

    Fragments

--- app/src/main/java/com/example/foodshala/MainActivity.kt
+++ app/src/main/java/com/example/foodshala/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,17 +14,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setFragment(HomeFragment())
         var token = getSharedPreferences("username", Context.MODE_PRIVATE)
 
-        username.text = token.getString("loginuser"," ")
+//        username.text = token.getString("loginuser"," ")
+//
+//        logout.setOnClickListener {
+//            var editor = token.edit()
+//            editor.putString("loginuser"," ")
+//            editor.commit()
+//             var intent = Intent(this,LogIn::class.java)
+//            startActivity(intent)
+//        }
 
-        logout.setOnClickListener {
-            var editor = token.edit()
-            editor.putString("loginuser"," ")
-            editor.commit()
-             var intent = Intent(this,LogIn::class.java)
-            startActivity(intent)
+        bottomNav.setOnItemSelectedListener {
+            when(it.itemId)
+            {
+                R.id.home -> setFragment(HomeFragment())
+                R.id.post -> setFragment(PostFragment())
+                R.id.Analytics -> setFragment(AnalyticsFragment())
+            }
+            return@setOnItemSelectedListener true
         }
+    }
 
+    private fun setFragment(fragment: Fragment){
+        val transaction =supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.FragmentFL, fragment)
+        transaction.disallowAddToBackStack()
+        transaction.commit()
     }
 }
